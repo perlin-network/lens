99a6930ef6d4f52932bef8c7e3f2ad9f7e648064
doug <39219081+doug-perlin@users.noreply.github.com>
2018-09-30 19:17:13 +0800

    sass: fix production css

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -228,6 +228,14 @@ module.exports = {
                         ),
                         // Note: this won't work without `new ExtractTextPlugin()` in `plugins`.
                     },
+                    {
+                        test: /\.scss$/,
+                        loaders: [
+                            require.resolve("style-loader"),
+                            require.resolve("css-loader"),
+                            require.resolve("sass-loader")
+                        ]
+                    },
                     // "file" loader makes sure assets end up in the `build` folder.
                     // When you `import` an asset, you get its filename.
                     // This loader doesn't use a "test" so it will catch all modules
