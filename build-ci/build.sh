8132597feedca2d3d1a56f6b6ce53107baa1875e
doug <39219081+doug-perlin@users.noreply.github.com>
2018-10-06 00:17:21 +0800

    build: ci build of website

---
+++ build-ci/build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -eu
+
+IMAGE_NAME="perlin-network/lens"
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+HOST_BUILD_DIR="$(cd ${SCRIPT_DIR}/../build && pwd)"
+
+cd ${SCRIPT_DIR}/..
+docker build -t ${IMAGE_NAME} -f ${SCRIPT_DIR}/Dockerfile .
+
+rm -rf ${HOST_BUILD_DIR}
+mkdir -p ${HOST_BUILD_DIR}
+CONTAINER_BUILD_DIR=$(docker run --rm ${IMAGE_NAME} bash -c "echo \$BUILD_DIR")
+docker run \
+    --rm \
+    --mount type=bind,source="${HOST_BUILD_DIR}",target="/host-build" \
+    ${IMAGE_NAME} \
+    bash -c "cp -R ${CONTAINER_BUILD_DIR}/* /host-build/ && \
+        # update the folder ownership from root to the script runner
+        chown $(id -u):$(id -g) -R /host-build/"
