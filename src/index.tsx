2c5b4b6f16ec2696553bb10f1f58354b58dbed0f
Daniel Hsing <hsing.daniel@gmail.com>
2019-01-24 20:54:10 -0500

    chore(styles): add ThemeProvider to project root and initialize theme breakpoints
    Signed-off-by: Daniel Hsing <hsing.daniel@gmail.com>

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,18 @@
-import 'normalize.css/normalize.css';
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-import App from './App';
-import './App.scss';
-import {Perlin} from "./Perlin";
-import registerServiceWorker from './registerServiceWorker';
-import {Store} from "./stores/Store";
-
+import "normalize.css/normalize.css";
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import App from "./App";
+import "./App.scss";
+import { Perlin } from "./Perlin";
+import registerServiceWorker from "./registerServiceWorker";
+import { Store } from "./stores/Store";
+import { ThemeProvider } from "styled-components";
+import { theme } from "./theme";
 
 ReactDOM.render(
-    <App perlin={new Perlin()} store={new Store()}/>,
-    document.getElementById('root') as HTMLElement
+    <ThemeProvider theme={theme}>
+        <App perlin={new Perlin()} store={new Store()} />
+    </ThemeProvider>,
+    document.getElementById("root") as HTMLElement
 );
 registerServiceWorker();
---
+++ src/theme.ts
@@ -0,0 +1,21 @@
+const theme = {
+    breakpoint: {
+        xs: "400px",
+        s: "600px",
+        m: "900px",
+        l: "1200px"
+    },
+    colors: {}
+};
+
+const invertTheme = {
+    breakpoint: {
+        xs: "400px",
+        s: "600px",
+        m: "900px",
+        l: "1200px"
+    },
+    colors: {}
+};
+
+export { theme, invertTheme };
