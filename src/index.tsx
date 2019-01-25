219ad5daa43531812926c4ca82516048ecb0a3fb
Daniel Hsing <hsing.daniel@gmail.com>
2019-01-24 22:07:03 -0500

    refactor(styles): move global styles into js using styled-components
    Signed-off-by: Daniel Hsing <hsing.daniel@gmail.com>

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,21 @@
 import "normalize.css/normalize.css";
+import "@blueprintjs/core/src/blueprint.scss";
+import "react-table/react-table.css";
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 import App from "./App";
-import "./App.scss";
 import { Perlin } from "./Perlin";
 import registerServiceWorker from "./registerServiceWorker";
 import { Store } from "./stores/Store";
 import { ThemeProvider } from "styled-components";
-import { theme } from "./theme";
+import { theme, GlobalStyle } from "./theme";
 
 ReactDOM.render(
     <ThemeProvider theme={theme}>
-        <App perlin={new Perlin()} store={new Store()} />
+        <>
+            <GlobalStyle />
+            <App perlin={new Perlin()} store={new Store()} />
+        </>
     </ThemeProvider>,
     document.getElementById("root") as HTMLElement
 );
