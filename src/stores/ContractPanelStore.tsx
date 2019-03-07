ccf275fd770b61949951bef280b5c2ac0b62387c
Daniel Hsing <hsing.daniel@gmail.com>
2019-03-06 21:14:13 -0500

    chore: remove unused panel/store components
    Signed-off-by: Daniel Hsing <hsing.daniel@gmail.com>

--- src/stores/ContractPanelStore.tsx
+++
@@ -1,7 +0,0 @@
-import { observable } from "mobx";
-
-class ContractPanelStore {
-    @observable public contractFile: string | any;
-}
-
-export { ContractPanelStore };
--- src/stores/Store.tsx
+++
@@ -1,9 +0,0 @@
-import { observable } from "mobx";
-
-class Store {
-    @observable public recipient: any;
-    @observable public amount: number;
-    @observable public contractFile: string | any;
-}
-
-export { Store };
