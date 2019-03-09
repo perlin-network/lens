4fb70a2aea60ad1b5857351334fa7993116feaa8
Yaya Lu <yaya@Yayas-MBP.lan>
2019-03-09 15:30:58 +1100

    Modified transaction table to use table tag instead of react-table

--- src/components/TransactionsTable.tsx
+++ src/components/TransactionsTable.tsx
@@ -1,12 +1,19 @@
 import * as React from "react";
-import ReactTable from "react-table";
+// import ReactTable from "react-table";
 import { Perlin } from "../Perlin";
 import { ITransaction, Tag } from "../types/Transaction";
 import { observer } from "mobx-react";
 import { formatDistance } from "date-fns";
+import "../index.scss";
+import { getJSDocTags } from "typescript";
 
 const columns = [
-    {
+    "Time",
+    "Transaction ID",
+    "Status",
+    "Creator",
+    "Tag"
+    /*{
         Header: "",
 
         id: "time",
@@ -51,27 +58,58 @@ const columns = [
             }
         },
         maxWidth: 80
-    }
+    }*/
 ];
 
 const perlin = Perlin.getInstance();
-
 @observer
 export default class TransactionsTable extends React.Component<{}, {}> {
     public render() {
-        return (
-            <ReactTable
-                data={perlin.transactions.recent}
-                columns={columns}
-                className="-striped -highlight"
-                defaultPageSize={15}
-                defaultSorted={[
-                    {
-                        id: "index",
-                        desc: true
-                    }
-                ]}
-            />
+        const dataColumns = columns;
+        const data = perlin.transactions.recent;
+        const tableHeaders = (
+            <thead>
+                <tr>
+                    {dataColumns.map(column => {
+                        console.log("column", { column });
+                        return <th key={column}>{column}</th>;
+                    })}
+                </tr>
+            </thead>
         );
+
+        const tableBody = data.map(tx => {
+            const date = new Date(tx.timestamp);
+            return (
+                <tr key={tx.id}>
+                    <td>
+                        {formatDistance(date, new Date(), { addSuffix: true })}
+                    </td>
+                    <td>{tx.id}</td>
+                    <td>{tx.status}</td>
+                    <td>{tx.creator}</td>
+                    <td>{getTag(tx)}</td>
+                </tr>
+            );
+        });
+
+        // Decorate with Bootstrap CSS
+        return (
+            <table className="tableStyle">
+                {tableHeaders} {tableBody}
+            </table>
+        );
+    }
+}
+function getTag(tx: ITransaction) {
+    switch (tx.tag) {
+        case Tag.NOP:
+            return "nop";
+        case Tag.TRANSFER:
+            return "transfer";
+        case Tag.CONTRACT:
+            return "contract";
+        case Tag.STAKE:
+            return "stake";
     }
 }
