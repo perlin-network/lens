c2634053077c0f32e3181cec7d99721432cb73a5
Daniel Hsing <hsing.daniel@gmail.com>
2019-03-10 13:55:55 -0400

    feat(contract): ui for rendering list of parameter inputs
    Signed-off-by: Daniel Hsing <hsing.daniel@gmail.com>

--- src/components/contract/ContractExecutor.tsx
+++ src/components/contract/ContractExecutor.tsx
@@ -4,9 +4,16 @@ import { Card } from "../common/core";
 import styled from "styled-components";
 import FunctionSelect from "./FunctionSelect";
 import ContractStore from "./ContractStore";
-import ParameterInput from "./ParameterInput";
+import ParameterInput, { ParamType } from "./ParameterInput";
 import { Button } from "../common/core";
 import { useComputed, observer } from "mobx-react-lite";
+import nanoid from "nanoid";
+
+interface IParamItem {
+    id: string;
+    type: ParamType | undefined;
+    value: string;
+}
 
 const contractStore = ContractStore.getInstance();
 const watFunctionRegex = /\(export "_contract_([a-zA-Z0-9_]+)" \(func \d+\)\)/g;
@@ -24,6 +31,54 @@ const useContractFunctions = () => {
     }, [contractStore.contract.textContent]);
 };
 
+const useParams = () => {
+    const [paramsList, setParamsList] = useState<IParamItem[]>([
+        { id: nanoid(), type: undefined, value: "" }
+    ]);
+    const setParamType = (id: string) => (type: ParamType) => {
+        setParamsList(prevList =>
+            prevList.map(item => {
+                if (item.id === id) {
+                    return {
+                        ...item,
+                        type
+                    };
+                }
+                return item;
+            })
+        );
+    };
+    const setParamValue = (id: string) => (value: string) => {
+        setParamsList(prevList =>
+            prevList.map(item => {
+                if (item.id === id) {
+                    return {
+                        ...item,
+                        value
+                    };
+                }
+                return item;
+            })
+        );
+    };
+    const deleteParam = (id: string) => () => {
+        setParamsList(prevList => prevList.filter(item => item.id !== id));
+    };
+    const addParam = () => {
+        setParamsList(prevList =>
+            prevList.concat({ id: nanoid(), type: undefined, value: "" })
+        );
+    };
+
+    return {
+        paramsList,
+        setParamValue,
+        setParamType,
+        deleteParam,
+        addParam
+    };
+};
+
 const Title = styled.h2`
     margin: 0;
     font-size: 16px;
@@ -36,9 +91,27 @@ const Wrapper = styled(Card).attrs({ showBoxShadow: false })`
     display: flex;
     flex-direction: column;
 `;
+const AddMoreText = styled.div`
+    cursor: pointer;
+    text-align: right;
+    margin: 0;
+    font-family: HKGrotesk;
+    font-size: 14px;
+    font-weight: 600;
+    color: #fff;
+    text-decoration: underline;
+    text-decoration-color: #fff;
+`;
 
 const ContractExecutor: React.SFC<{}> = observer(() => {
     const funcList = useContractFunctions();
+    const {
+        paramsList,
+        setParamValue,
+        setParamType,
+        deleteParam,
+        addParam
+    } = useParams();
     const [currFunc, setFunc] = useState("");
     useEffect(() => {
         setFunc(funcList[0]);
@@ -56,7 +129,17 @@ const ContractExecutor: React.SFC<{}> = observer(() => {
                 value={currFunc}
             />
             <Title style={{ marginTop: "25px" }}>Specify Parameters</Title>
-            <ParameterInput />
+            {paramsList.map(paramItem => (
+                <ParameterInput
+                    key={paramItem.id}
+                    value={paramItem.value}
+                    type={paramItem.type}
+                    onChange={setParamValue(paramItem.id)}
+                    onTypeChange={setParamType(paramItem.id)}
+                    onDelete={deleteParam(paramItem.id)}
+                />
+            ))}
+            <AddMoreText onClick={addParam}>Add more parameters</AddMoreText>
             <Button fontSize="14px">Call Function</Button>
         </Wrapper>
     );
--- src/components/contract/ParameterInput.tsx
+++ src/components/contract/ParameterInput.tsx
@@ -1,18 +1,79 @@
 import * as React from "react";
 import styled from "styled-components";
+import { Input } from "../common/core";
 import { Flex } from "@rebass/grid";
-import Dropdown from "react-dropdown";
+import Dropdown, { Option } from "react-dropdown";
+import CloseIconSVG from "../../assets/svg/close-icon.svg";
 
-const typeOptions = ["String", "Uint16", "Uint32", "Uint64", "Bytes", "Byte"];
+interface IParameterInputProps {
+    onDelete: () => void;
+    onChange: (value: string) => void;
+    onTypeChange: (type: ParamType) => void;
+    value: string;
+    type: ParamType | undefined;
+}
+
+export enum ParamType {
+    String = "String",
+    Uint16 = "Uint16",
+    Uint32 = "Uint32",
+    Uint64 = "Uint64",
+    Bytes = "Bytes",
+    Byte = "Byte"
+}
+
+const typeOptions = [
+    ParamType.String,
+    ParamType.Uint16,
+    ParamType.Uint32,
+    ParamType.Uint64,
+    ParamType.Bytes,
+    ParamType.Byte
+];
 
 const Wrapper = styled(Flex)`
     margin-bottom: 30px;
 `;
+const StyledInput = styled(Input)`
+    margin-left: 10px;
+    margin-right: 15px;
+    flex-grow: 1;
+`;
+const DeleteIcon = styled.img.attrs({ src: CloseIconSVG })`
+    height: 15px;
+    width: 15px;
+    object-fit: contain;
+    cursor: pointer;
+`;
 
-const ParameterInput: React.SFC<{}> = () => {
+const ParameterInput: React.SFC<IParameterInputProps> = ({
+    onDelete,
+    onChange,
+    onTypeChange,
+    value,
+    type
+}) => {
+    const handleTypeChange = (option: Option) => {
+        onTypeChange(option.value as ParamType);
+    };
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        onChange(e.target.value);
+    };
+
     return (
         <Wrapper alignItems="center">
-            <Dropdown options={typeOptions} placeholder="Select a Type" />
+            <Dropdown
+                options={typeOptions}
+                placeholder="Select a Type"
+                value={type}
+                onChange={handleTypeChange}
+            />
+            <StyledInput
+                placeholder="Add parameter here"
+                value={value}
+                onChange={handleChange}
+            />
+            <DeleteIcon onClick={onDelete} />
         </Wrapper>
     );
 };
