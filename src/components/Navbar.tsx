35252842a4207019fb3d9f8501ba4964a2498338
Daniel Hsing <hsing.daniel@gmail.com>
2019-03-26 20:18:50 -0400

    feat(nav): merge navbar branch
    Signed-off-by: Daniel Hsing <hsing.daniel@gmail.com>

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,12 +1,19 @@
 import * as React from "react";
 import styled from "styled-components";
+import "../index.scss";
 import perlinLogo from "../assets/svg/perlin-logo.svg";
 import { Flex, Box } from "@rebass/grid";
+import { Perlin } from "../Perlin";
+import { QRCode } from "react-qr-svg";
+import { observer } from "mobx-react";
 
 const Wrapper = styled(Flex)`
     margin: 46px 40px;
 `;
 
+const perlin = Perlin.getInstance();
+
+@observer
 export default class Navbar extends React.Component<{}, {}> {
     public render() {
         return (
@@ -14,7 +21,20 @@ export default class Navbar extends React.Component<{}, {}> {
                 <Box width={1 / 6}>
                     <img src={perlinLogo} style={{ width: "12em" }} />
                 </Box>
-                <Box width={5 / 6} />
+                <Box width={3 / 6} />
+                <Box width={2 / 6}>
+                    <div className="QR-grid">
+                        <div className="QR-grid-row1">
+                            {perlin.publicKeyHex}
+                        </div>
+                        <div className="QR-grid-row2">
+                            <QRCode
+                                value={perlin.publicKeyHex}
+                                style={{ width: "100%", height: "100%" }}
+                            />
+                        </div>
+                    </div>
+                </Box>
             </Wrapper>
         );
     }
