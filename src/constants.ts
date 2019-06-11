c2b4cd4ae7c78dac3ca48f916a20c7df9c7a63dc
Pisuth Daengthongdee <pisuth.dae@gmail.com>
2019-06-11 23:45:12 +0700

    feat: new storage key for the wallet

--- src/constants.ts
+++ src/constants.ts
@@ -2,7 +2,8 @@ export const STORAGE_KEYS = {
     STORED_HOSTS: "storedHosts",
     CURRENT_HOST: "currentHost",
     TRANSACTION_GRAPH_NODES_LIMIT: "transactionGraphNodeLimit",
-    NETWORK_GRAPH_NODES_LIMIT: "networkGraphNodeLimit"
+    NETWORK_GRAPH_NODES_LIMIT: "networkGraphNodeLimit",
+    SECRET_KEY: "secretKey"
 };
 export const DEFAULT_API_HOST = location.hostname + ":9000";
 export const DEFAULT_TRANSACTION_GRAPH_NODES_LIMIT = 500;
