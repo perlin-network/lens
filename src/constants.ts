e4aa2fb34407bccbe323bdb128ffb0d45dac9b46
Claudiu Filip <claudiu@perlin.net>
2019-10-27 22:43:19 +0200

    Allow custom protocols for api host and web socket (#40)
    * perlin: included protocol for configuratble host fields
    
    * deploy: simplified deployment to github pages
    
    * deploy: extracted 'lens' subfolder to dedicated deploy:subfolder
    
    * host: added backward compatibility with previously set hosts

--- src/constants.ts
+++ src/constants.ts
@@ -1,14 +1,13 @@
 export const STORAGE_KEYS = {
     STORED_HOSTS: "storedHosts",
-    CURRENT_HOST: "currentHost",
+    CURRENT_HOST: "host",
     TRANSACTION_GRAPH_NODES_LIMIT: "transactionGraphNodeLimit",
     NETWORK_GRAPH_NODES_LIMIT: "networkGraphNodeLimit",
     SECRET_KEY: "secretKey"
 };
-export const DEFAULT_API_HOST = location.hostname + ":9000";
-export const HTTPS = !!process.env.REACT_APP_HTTPS;
-export const HTTP_PROTOCOL = HTTPS ? "https" : "http";
-export const WS_PROTOCOL = HTTPS ? "wss" : "ws";
+export const DEFAULT_API_HOST =
+    process.env.REACT_APP_DEFAULT_API_HOST ||
+    `${location.protocol}//${location.hostname}:9000`;
 export const DEFAULT_TRANSACTION_GRAPH_NODES_LIMIT = 500;
 export const TX_FEE = 2;
 export const DEFAULT_NETWORK_GRAPH_NODES_LIMIT = 500;
